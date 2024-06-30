23f85c833878be0d04aa97614b67cc7265f33a81
Khushi gupta <112059973+khusiie@users.noreply.github.com>
2024-06-30 17:06:34 +0530

    Add files via upload

---
+++ connectMongoDB.js
@@ -0,0 +1,18 @@
+
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
